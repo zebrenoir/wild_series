d93e86d0c0011b806cb81040ec6f786c210ea47b
Sofiane Daouzli <sofianesk8@hotmail.com>
2019-11-18 15:59:22 +0100

    finish quest Symfony 04

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,28 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+
+// require the JavaScript
+require('bootstrap-star-rating');
+// require 2 CSS files needed
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-svg/theme.css');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
